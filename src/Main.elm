af4c1148f46022f4a365b484326b8c047b7d7a40
dyoshikawa <dyoshikawa1993@gmail.com>
2019-11-07 13:09:39 +0900

    init

---
+++ src/Main.elm
@@ -0,0 +1,137 @@
+module Main exposing (Model, Msg(..), init, main, subscriptions, update, view, viewLink)
+
+import Browser
+import Browser.Navigation as Nav
+import Html exposing (..)
+import Html.Attributes exposing (..)
+import Url
+import Url.Parser exposing ((</>), Parser, int, map, oneOf, parse, s, string)
+
+
+
+-- MAIN
+
+
+main : Program () Model Msg
+main =
+    Browser.application
+        { init = init
+        , view = view
+        , update = update
+        , subscriptions = subscriptions
+        , onUrlChange = UrlChanged
+        , onUrlRequest = LinkClicked
+        }
+
+
+
+-- MODEL
+
+
+type Route
+    = Top
+    | About
+    | NotFound
+
+
+type alias Model =
+    { key : Nav.Key
+    , route : Route
+    }
+
+
+init : () -> Url.Url -> Nav.Key -> ( Model, Cmd Msg )
+init flags url key =
+    ( Model key Top, Cmd.none )
+
+
+
+-- UPDATE
+
+
+type Msg
+    = LinkClicked Browser.UrlRequest
+    | UrlChanged Url.Url
+
+
+update : Msg -> Model -> ( Model, Cmd Msg )
+update msg model =
+    case msg of
+        LinkClicked urlRequest ->
+            case urlRequest of
+                Browser.Internal url ->
+                    ( model, Nav.pushUrl model.key (Url.toString url) )
+
+                Browser.External href ->
+                    ( model, Nav.load href )
+
+        UrlChanged url ->
+            let
+                route =
+                    parseUrl url
+            in
+            ( { model | route = route }
+            , Cmd.none
+            )
+
+
+routeParser : Parser (Route -> a) a
+routeParser =
+    oneOf
+        [ Url.Parser.map Top (Url.Parser.s "")
+        , Url.Parser.map About (Url.Parser.s "about")
+        ]
+
+
+parseUrl : Url.Url -> Route
+parseUrl url =
+    let
+        parsed =
+            parse routeParser url
+    in
+    case parsed of
+        Just route ->
+            route
+
+        Nothing ->
+            NotFound
+
+
+
+-- SUBSCRIPTIONS
+
+
+subscriptions : Model -> Sub Msg
+subscriptions _ =
+    Sub.none
+
+
+
+-- VIEW
+
+
+view : Model -> Browser.Document Msg
+view model =
+    { title = "URL Interceptor"
+    , body =
+        [ text "The current URL is: "
+        , ul []
+            [ viewLink "/top"
+            , viewLink "/about"
+            ]
+        , case model.route of
+            Top ->
+                div [] [ text "Top" ]
+
+            About ->
+                div [] [ text "About" ]
+
+            NotFound ->
+                div [] [ text "NotFound" ]
+        ]
+    }
+
+
+viewLink : String -> Html msg
+viewLink path =
+    li [] [ a [ href path ] [ text path ] ]
